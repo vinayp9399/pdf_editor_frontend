940c2830ae942de2545b66c325dca4ead8915d08
Vinay Phatarpekar <vinayp9399@gmail.com>
2025-01-01 11:01:12 +0530

    done

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -597,7 +597,8 @@ return () => {
           <>
           <div style={{position:"relative", display:"flex", justifyContent:"center"}}>
           <div style={{position:"absolute",border:"2px solid #4245a8", height:"430px", width:"1220px",display:"flex",justifyContent:"center", alignItems:"center",zIndex: 1,marginLeft:"auto",marginTop:"90px",marginRight:"auto"}}>
-          Loading....
+          <div className="loader">
+          </div>
           </div></div>
 
           </>
